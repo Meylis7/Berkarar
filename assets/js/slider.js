d7892baf2cdd0f2be7344a4a595a81652a01c880
Meylis Gazakow <Meylis7@users.noreply.github.com>
2022-08-24 00:20:26 +0500

    responsive done

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -68,50 +68,40 @@ $(".movie_slider").slick({
     ]
 });
 
-
-// $(".project_box").slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     arrows: true,
-//     autoplay: true,
-//     dots: true,
-//     autoplaySpeed: 3000,
-//     pauseOnHover: true,
-//     centerMode: true,
-//     centerPadding: '15%',
+$(".post_slider").slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 8000,
+    pauseOnHover: false,
 
 
-//     infinite: true,
-//     speed: 800,
-//     fade: false,
-//     adaptiveHeight: false,
-//     lazyLoad: 'ondemand',
+    infinite: true,
+    speed: 800,
+    fade: false,
+    adaptiveHeight: true,
+    lazyLoad: 'ondemand',
 
-//     cssEase: 'linear',
-//     swipeToSlide: true,
-//     waitForAnimate: false,
-//     draggable: true,
+    cssEase: 'linear',
+    swipeToSlide: true,
+    waitForAnimate: false,
+    draggable: true,
 
-//     responsive: [
-//         {
-//             breakpoint: 1270,
-//             settings: {
-//                 centerPadding: '5%',
-//                 arrows: false,
-//             }
-//         },
-//         {
-//             breakpoint: 900,
-//             settings: {
-//                 slidesToShow: 1,
-//                 slidesToScroll: 1,
-//             }
-//         },
-//         {
-//             breakpoint: 800,
-//             settings: {
-//                 centerPadding: '3.4%',
-//             }
-//         }
-//     ]
-// });
+    responsive: [
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 1,
+            }
+        },
+
+    ]
+});
